c505733cb791d23223dcef00804baa6b6fdfe87f
H0ngJu <hongju4385@gmail.com>
2024-02-02 16:03:22 +0900

    #1-임시 상단바 추가

--- lib/screen/HomePatient.dart
+++ lib/screen/HomePatient.dart
@@ -1,6 +1,4 @@
-// 홈 화면
 import 'package:flutter/material.dart';
-
 
 class HomePatient extends StatelessWidget {
   const HomePatient({Key? key}) : super(key: key);
@@ -8,12 +6,28 @@ class HomePatient extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      backgroundColor: Colors.black38,
-      body: Container(child: Text(
-      'Hello',
-      style: TextStyle(
-        fontSize: 24,
-        color: Colors.white)))
-    );
+        backgroundColor: Colors.white,
+        appBar: AppBar(
+          backgroundColor: Colors.white,
+          centerTitle: true,
+          title: Image.asset('lib/assets/logo.png', fit: BoxFit.cover),
+          actions: <Widget>[
+            IconButton(
+                onPressed: () {},
+                icon: Icon(
+                  Icons.notifications,
+                  color: Colors.black,
+                )),
+            IconButton(
+                onPressed: () {},
+                icon: Icon(
+                  Icons.person,
+                  color: Colors.black,
+                ))
+          ],
+        ),
+        body: Container(
+            child: Text('Hello',
+                style: TextStyle(fontSize: 24, color: Colors.white))));
   }
 }
