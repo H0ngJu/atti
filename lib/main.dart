1eb18bf3be9b80d314b69784c88dd1e90fbd83c4
H0ngJu <hongju4385@gmail.com>
2024-02-08 15:38:48 +0900

    #12-보호자 홈 ui 완성

--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,4 @@
+import 'package:atti/screen/HomeCarer.dart';
 import 'package:atti/screen/LogInSignUp/LogInSignUpMainScreen.dart';
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
@@ -30,7 +31,7 @@ class MyApp extends StatelessWidget {
       ],
       debugShowCheckedModeBanner: false,
       theme: ThemeData(),
-      home: HomePatient(),
+      home: HomeCarer(),
     );
   }
 }
