09197ad06f0f99209f75ccb68c27ef23f77c4f85
H0ngJu <hongju4385@gmail.com>
2024-02-15 11:49:39 +0900

    #14-회상대화 ui

--- lib/screen/memory/chat/Chat.dart
+++ lib/screen/memory/chat/Chat.dart
@@ -1,3 +1,7 @@
+import 'dart:async';
+import 'package:atti/commons/SimpleAppBar.dart';
+import 'package:atti/screen/memory/chat/ChatBubble.dart';
+import 'package:permission_handler/permission_handler.dart';
 import 'package:flutter/material.dart';
 import 'package:speech_to_text/speech_to_text.dart' as stt;
 
@@ -12,9 +16,9 @@ class _ChatState extends State<Chat> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(title: Text('아띠와 회상 대화')),
-      body: SingleChildScrollView(
-        child: Container(
+      appBar: SimpleAppBar(title: '아띠와의 회상 대화'),
+      body: Stack(children: [
+        Container(
           margin: EdgeInsets.all(16),
           child: Column(
             mainAxisAlignment: MainAxisAlignment.start,
@@ -34,79 +38,283 @@ class _ChatState extends State<Chat> {
                   )
                 ],
               ),
+              ChatBubble(),
               Row(
                 mainAxisAlignment: MainAxisAlignment.center,
                 children: [
                   Image(
                     image: AssetImage('lib/assets/Atti/Normal.png'),
-                    width: 320,
+                    width: MediaQuery.of(context).size.width * 0.65,
                   ),
                 ],
               ),
-              Row(
-                crossAxisAlignment: CrossAxisAlignment.center,
-                mainAxisAlignment: MainAxisAlignment.center,
-                children: [],
-              ),
-              VoiceToTextDemo(),
+              VoiceButton(),
             ],
           ),
         ),
-      ),
+        Positioned(
+          bottom: 0,
+          left: MediaQuery.of(context).size.width * 0.05,
+          right: MediaQuery.of(context).size.width * 0.05,
+          child: SlideUpPanel(),
+        ),
+      ]),
     );
   }
 }
 
-class VoiceToTextDemo extends StatefulWidget {
+// 대화기록, 마이크, 대화 종료 버튼
+class VoiceButton extends StatefulWidget {
+  const VoiceButton({Key? key}) : super(key: key);
+
   @override
-  _VoiceToTextDemoState createState() => _VoiceToTextDemoState();
+  State<VoiceButton> createState() => _VoiceButtonState();
 }
 
-class _VoiceToTextDemoState extends State<VoiceToTextDemo> {
-  final stt.SpeechToText _speech = stt.SpeechToText();
+class _VoiceButtonState extends State<VoiceButton> {
+  stt.SpeechToText _speech = stt.SpeechToText();
+  String _spokenText = '버튼을 누르고 음성을 입력';
   bool _isListening = false;
-  String _text = '';
+  int _staticTimeout = 2; // 정적 상태 타임아웃 (2초)
+  int _elapsedTime = 0;
+
+  @override
+  void initState() {
+    super.initState();
+    _requestMicrophonePermission(); // 페이지가 처음 로딩될 때 권한 요청
+  }
+
+  void _requestMicrophonePermission() async {
+    // 마이크 권한 요청
+    var status = await Permission.microphone.request();
+    if (status.isDenied) {
+      // 사용자가 권한을 거부한 경우 처리
+      print('마이크 권한이 거부되었습니다.');
+    } else if (status.isPermanentlyDenied) {
+      // 사용자가 권한을 영구적으로 거부한 경우 처리
+      print('마이크 권한이 영구적으로 거부되었습니다.');
+      await openAppSettings(); // 앱 설정으로 이동, 직접 권한을 설정
+    }
+  }
+
+  void _startListening() async {
+    bool available = await _speech.initialize(
+      onStatus: (status) {},
+      onError: (error) {},
+    );
+
+    if (available) {
+      _speech.listen(
+        onResult: (result) {
+          setState(() {
+            _spokenText = result.recognizedWords;
+            _resetStaticTimer(); // 음성인식 후 타이머 리셋
+          });
+        },
+      );
+    } else {
+      print("Speech recognition not available");
+    }
+  }
+
+  void _resetStaticTimer() {
+    setState(() {
+      _elapsedTime = 0;
+    });
+  }
+
+  void _startStaticTimer() {
+    _resetStaticTimer();
+    // 1초마다 정적 시간을 증가 -> 타이머 시작.
+    Timer.periodic(Duration(seconds: 1), (timer) {
+      setState(() {
+        _elapsedTime++;
+        if (_elapsedTime >= _staticTimeout) {
+          _stopListening(); // 정적 타임아웃이 발생하면 음성 입력 중지
+          timer.cancel(); // 타이머 중지
+        }
+      });
+    });
+  }
+
+  void _stopListening() {
+    if (_isListening) {
+      _speech.stop();
+      setState(() {
+        _isListening = false;
+      });
+    }
+  }
+
+  void _toggleListening() {
+    if (_isListening) {
+      _stopListening();
+    } else {
+      _startListening();
+      _startStaticTimer();
+    }
+    setState(() {
+      _isListening = !_isListening; // 버튼 상태 변경
+    });
+  }
 
   @override
   Widget build(BuildContext context) {
-    Color backgroundColor = _isListening ? Colors.green : Colors.red;
+    return Column(children: [
+      Row(
+          crossAxisAlignment: CrossAxisAlignment.center,
+          mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+          children: [
+            Container(
+              height: MediaQuery.of(context).size.width * 0.2,
+              margin: EdgeInsets.only(top: 20),
+              child: ElevatedButton(
+                  onPressed: () {},
+                  style: ElevatedButton.styleFrom(
+                      backgroundColor: Color(0xffFFF5DB),
+                      shape: CircleBorder()),
+                  child: Text('대화\n기록',
+                      style: TextStyle(color: Color(0xffA38130)))),
+            ),
+            Container(
+              height: MediaQuery.of(context).size.width * 0.2,
+              margin: EdgeInsets.only(top: 20),
+              child: ElevatedButton(
+                onPressed: _isListening ? _stopListening : _toggleListening,
+                style: ElevatedButton.styleFrom(
+                    backgroundColor:
+                        _isListening ? Color(0xff231FAD) : Color(0xffFFC215),
+                    shape: CircleBorder()),
+                child: Icon(
+                  _isListening ? Icons.stop : Icons.mic,
+                  size: 40,
+                  color: Colors.white,
+                ),
+              ),
+            ),
+            Container(
+              height: MediaQuery.of(context).size.width * 0.2,
+              margin: EdgeInsets.only(top: 20),
+              child: ElevatedButton(
+                  onPressed: () {},
+                  style: ElevatedButton.styleFrom(
+                      backgroundColor: Color(0xffFFF5DB),
+                      shape: CircleBorder()),
+                  child: Text(
+                    '대화\n종료',
+                    style: TextStyle(color: Color(0xffA38130)),
+                  )),
+            ),
+          ]),
 
-    return Center(
-      child: Column(
-        mainAxisAlignment: MainAxisAlignment.center,
-        children: [
-          FloatingActionButton(
-            onPressed: _toggleListening,
-            child: Icon(_isListening ? Icons.mic : Icons.mic_none),
-          ),
-          SizedBox(height: 20),
-          Text(
-            _text,
-            style: TextStyle(fontSize: 24),
-          ),
-        ],
+      // 얘는 임시로 말하는거 보여주려고 ..
+      Container(
+          width: MediaQuery.of(context).size.width * 0.8,
+          padding: EdgeInsets.all(10),
+          decoration: BoxDecoration(
+              border: Border.all(width: 1, color: Color(0xffF6B818))),
+          child: Text('$_spokenText',
+              style: TextStyle(color: Colors.black, fontSize: 25)))
+    ]);
+  }
+}
+
+// 슬라이드 업
+class SlideUpPanel extends StatefulWidget {
+  const SlideUpPanel({Key? key}) : super(key: key);
+
+  @override
+  State<SlideUpPanel> createState() => _SlideUpPanelState();
+}
+
+class _SlideUpPanelState extends State<SlideUpPanel> {
+  bool _isPanelOpen = false;
+  double _panelHeightClosed = 50;
+  double _panelHeightOpen = 50;
+  final List<String> tagList = ['아들', '손자', '돌잔치', '아들', '손자', '돌잔치'];
+
+
+  Widget TagContainer(tagName) {
+    return Container(
+      margin: EdgeInsets.all(2),
+      decoration: BoxDecoration(
+          color: Color(0xffFFF5DB), borderRadius: BorderRadius.circular(15)),
+      padding: EdgeInsets.all(10),
+      child: Text(
+        '$tagName',
+        style: TextStyle(color: Color(0xffA38130), fontSize: 24),
       ),
     );
   }
 
-  void _toggleListening() async {
-    if (!_isListening) {
-      bool available = await _speech.initialize();
-      if (available) {
+  @override
+  Widget build(BuildContext context) {
+    _panelHeightOpen = MediaQuery.of(context).size.height * 0.55;
+    _panelHeightClosed = MediaQuery.of(context).size.height * 0.12;
+    return GestureDetector(
+      onTap: () {
         setState(() {
-          _isListening = true;
-          _speech.listen(
-            onResult: (result) => setState(() {
-              _text = result.recognizedWords;
-            }),
-          );
+          _isPanelOpen = !_isPanelOpen;
         });
-      }
-    } else {
-      setState(() {
-        _isListening = false;
-        _speech.stop();
-      });
-    }
+      },
+      child: AnimatedContainer(
+          duration: Duration(milliseconds: 500),
+          height: _isPanelOpen ? _panelHeightOpen : _panelHeightClosed,
+          decoration: BoxDecoration(
+              color: Color(0xffFFE9B3),
+              borderRadius: BorderRadius.only(
+                  topLeft: Radius.circular(30), topRight: Radius.circular(30))),
+          child: _isPanelOpen
+              ? Container(
+                  child: Column(
+                    children: [
+                      Icon(
+                        Icons.keyboard_arrow_down_rounded,
+                        size: 50,
+                        color: Color(0xffFFC215),
+                      ),
+                      Text(
+                        '사진 닫기',
+                        style:
+                            TextStyle(fontSize: 20, color: Color(0xffA38130)),
+                      ),
+                      SizedBox(
+                        height: MediaQuery.of(context).size.height * 0.01,
+                      ),
+                      Image.network(
+                        'https://newsimg-hams.hankookilbo.com/2022/05/08/f5107e5a-7266-4132-9550-8713162df25a.jpg',
+                        fit: BoxFit.cover,
+                        width: MediaQuery.of(context).size.width * 0.7,
+                        height: MediaQuery.of(context).size.height * 0.2,
+                      ),
+                      Container(
+                        margin: EdgeInsets.only(top: 10),
+                        child: Wrap(
+                          spacing: 10, // 각 행의 간격을 조절합니다.
+                          children: tagList.map((tag) {
+                            return TagContainer(tag);
+                          }).toList(),
+                        ),
+                      ),
+                    ],
+                  ),
+                )
+              : Container(
+                  child: Column(
+                    children: [
+                      Icon(
+                        Icons.keyboard_arrow_up_rounded,
+                        size: 50,
+                        color: Color(0xffFFC215),
+                      ),
+                      Text(
+                        '사진 보기',
+                        style:
+                            TextStyle(fontSize: 20, color: Color(0xffA38130)),
+                      )
+                    ],
+                  ),
+                )),
+    );
   }
 }
