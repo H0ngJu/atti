85fb9ae6ab6b763c4b12ba4ecc65a56395d6c8b4
sujeong <qkrtnwjd4212@naver.com>
2024-04-27 22:01:34 +0900

    feat/#92/멀티모달로 변경

--- lib/screen/chatbot/Chatbot.dart
+++ lib/screen/chatbot/Chatbot.dart
@@ -1,4 +1,5 @@
 import 'dart:convert';
+import 'dart:typed_data';
 import 'package:flutter_dotenv/flutter_dotenv.dart';
 import 'package:http/http.dart' as http;
 import 'package:flutter/material.dart';
@@ -49,9 +50,17 @@ import 'package:google_generative_ai/google_generative_ai.dart';
 // }
 
 class Chatbot {
-  Future<String> getResponse(var prompt) async {
-    print('Received prompt: $prompt');
+  // 이미지 URL을 다운로드하고 바이트 데이터로 반환
+  Future<List<int>> _downloadImage(String url) async {
+    final response = await http.get(Uri.parse(url));
+    if (response.statusCode == 200) {
+      return response.bodyBytes;
+    } else {
+      throw Exception('이미지 다운로드 실패: ${response.statusCode}');
+    }
+  }
 
+  Future<String> getResponse(var prompt, String img) async {
     await dotenv.load(fileName: '.env');
     final String apiKey = dotenv.env['GEMINI_API_KEY']!;
     if (apiKey == null) {
@@ -67,16 +76,44 @@ class Chatbot {
             temperature: 0.7,
         ),
       requestOptions: RequestOptions(apiVersion: 'v1beta'),
+      systemInstruction: Content.text(
+          '너는 치매 어르신과 대화를 나누는 보이스봇 아띠야. 어르신이 화면 속의 사진에 담긴 추억을 회상하는 것을 돕는 것이 목적이야. 너의 역할은 질문에 대한 정보를 제공하는 것이 아니야. 어르신께 사진에 대해 물어봐줘. 공감하고 긍정적인 정서를 이끌어내도록 어르신께 대화를 유도해줘. 그리고 너무 길게 말하지 말아줘.'
+      ),
     );
+
+    // 이미지 처리
+    final visionModel = GenerativeModel(
+      model: 'gemini-pro-vision',
+      apiKey: apiKey,);
+    var startTime = DateTime.now(); //
+    final userMemoryImg = Uint8List.fromList(await _downloadImage(img));
+    var endTime = DateTime.now(); //
+    print('이미지 다운로드 시간: ${endTime.difference(startTime)}');
+
+    final imgPrompt = TextPart("이미지에 대해 자세히 설명해줘.");
+    final imageParts = [DataPart('image/jpeg', userMemoryImg)];
+    startTime = DateTime.now();
+    final imgDescription = await visionModel.generateContent([Content.multi([imgPrompt, ...imageParts])]);
+    endTime = DateTime.now();
+    print('이미지 설명 생성 시간: ${endTime.difference(startTime)}');
+    print(imgDescription.text);
+
     final chat = model.startChat(history: [
-      Content.text('너는 치매 어르신과 대화를 나누는 보이스봇 아띠야. 어르신이 화면 속의 사진에 담긴 추억을 회상하는 것을 돕는 것이 목적이야. 너의 역할은 질문에 대한 정보를 제공하는 것이 아니야. 어르신께 사진에 대해 물어봐줘. 공감하고 긍정적인 정서를 이끌어내도록 어르신께 대화를 유도해줘.'),
+      Content.text(imgDescription.text!),
       Content.model([TextPart('어르신, 어떤 날 찍은 사진인지 기억하시나요? 이 때의 기분은 어떠셨어요?')]),
     ]);
     //var content = Content.text('How many paws are in my house?');
+    startTime = DateTime.now();
     var response = await chat.sendMessage(Content.text(prompt));
+    endTime = DateTime.now();
+    print('챗봇 응답 생성 시간: ${endTime.difference(startTime)}');
     print(response.text);
     return response.text!;
   }
+
+
+
+
 }
 
 // class ChatScreen extends StatefulWidget {
