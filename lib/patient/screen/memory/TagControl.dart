468aff687bd6cdc38a76a9c07106e0581487bee2
H0ngJu <hongju4385@gmail.com>
2024-12-15 21:06:48 +0900

    #195 - main memory 리팩토링

---
+++ lib/patient/screen/memory/TagControl.dart
@@ -0,0 +1,6 @@
+import 'package:get/get.dart';
+import 'package:get/get_state_manager/src/simple/get_controllers.dart';
+
+class TagControl extends GetxController {
+  var selectedTag = '연도'.obs; // 선택된 태그를 저장하는 Observable
+}
