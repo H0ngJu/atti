f86ab674e607db07465da1a0dfe50bcc24d67199
H0ngJu <hongju4385@gmail.com>
2024-12-19 20:01:34 +0900

    #195 - 앨범 리스트 로직 수정(5개씩 묶어서 배치)

--- lib/patient/screen/memory/MemoryAlbum.dart
+++ lib/patient/screen/memory/MemoryAlbum.dart
@@ -16,62 +16,141 @@ class MemoryAlbum extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: SimpleAppBar(title: "$memoryKey\n기억모음"),
-      body: Stack(children: [
-        Padding(
-          padding: const EdgeInsets.all(16.0),
-          child: Column(
-            crossAxisAlignment: CrossAxisAlignment.start,
-            children: [
-              Expanded(
-                child: GridView.builder(
-                  gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
-                    crossAxisCount: 2,
-                    crossAxisSpacing: 8.0,
-                    mainAxisSpacing: 8.0,
-                    childAspectRatio:
-                        0.8,
-                  ),
-                  itemCount: group.length,
-                  itemBuilder: (context, index) {
-                    final groupedMemory = group[index];
-                    return GestureDetector(
-                      onTap: () => Get.to(MemoryDetail(memory: groupedMemory)),
-                      child: Container(
-                          margin: const EdgeInsets.symmetric(
-                              vertical: 8, horizontal: 16),
-                          child: Column(children: [
-                            ClipRRect(
-                                borderRadius: BorderRadius.circular(20.0),
-                                child: Column(children: [
-                                  Image.network(
-                                    groupedMemory.img ?? '',
-                                    width:
-                                        MediaQuery.of(context).size.width * 0.4,
-                                    height:
-                                        MediaQuery.of(context).size.width * 0.4,
-                                    fit: BoxFit.cover,
-                                  ),
-                                ])),
-                            Text(
-                              groupedMemory.imgTitle ?? '',
-                              style: const TextStyle(
-                                  color: Colors.black,
-                                  fontSize: 24,
-                                  fontFamily: 'PretendardRegular'),
-                            ),
-                          ])),
-                    );
-                  },
+      body: Stack(
+        children: [
+          Padding(
+            padding: const EdgeInsets.all(16.0),
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                Expanded(
+                  child: group.length > 5
+                      ? _buildGroupedMemoryCards()
+                      : _buildGridView(context),
                 ),
-              ),
-            ],
+              ],
+            ),
           ),
-        ),
-        Positioned(
-            bottom: MediaQuery.of(context).size.height * 0.2,
-            right: 0,
-            child: AddButton())
-      ]),
+          Positioned(
+              bottom: MediaQuery.of(context).size.height * 0.2,
+              right: 0,
+              child: AddButton())
+        ],
+      ),
     );
   }
+
+  Widget _buildGridView(BuildContext context) {
+    return GridView.builder(
+      gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
+        crossAxisCount: 2,
+        crossAxisSpacing: 8.0,
+        mainAxisSpacing: 8.0,
+        childAspectRatio: 0.8,
+      ),
+      itemCount: group.length,
+      itemBuilder: (context, index) {
+        final groupedMemory = group[index];
+        return _buildMemoryCard(context, groupedMemory);
+      },
+    );
+  }
+
+  Widget _buildGroupedMemoryCards() {
+    return ListView.builder(
+      itemCount: (group.length / 5).ceil(), // 5개씩 묶기
+      shrinkWrap: true,
+      physics: const BouncingScrollPhysics(),
+      itemBuilder: (context, blockIndex) {
+        final startIndex = blockIndex * 5;
+        final endIndex = (startIndex + 5 <= group.length)
+            ? startIndex + 5
+            : group.length; // 남은 4개 아이템
+        final currentGroup = group.sublist(startIndex, endIndex);
+
+        return Column(
+          children: [
+            if (currentGroup.isNotEmpty)
+              Padding(
+                padding: const EdgeInsets.symmetric(vertical: 8.0),
+                child: _buildBigMemoryCard(context, currentGroup[0]),
+              ),
+            if (currentGroup.length > 1)
+              GridView.builder(
+                gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
+                  crossAxisCount: 2,
+                  crossAxisSpacing: 8.0,
+                  mainAxisSpacing: 8.0,
+                  childAspectRatio: 0.8,
+                ),
+                itemCount: currentGroup.length - 1,
+                shrinkWrap: true,
+                physics: const NeverScrollableScrollPhysics(),
+                itemBuilder: (context, gridIndex) {
+                  final groupedMemory = currentGroup[gridIndex + 1];
+                  return _buildMemoryCard(context, groupedMemory);
+                },
+              ),
+          ],
+        );
+      },
+    );
+  }
+
+  Widget _buildMemoryCard(BuildContext context, MemoryNoteModel groupedMemory) {
+    return GestureDetector(
+      onTap: () => Get.to(MemoryDetail(memory: groupedMemory)),
+      child: Column(
+        children: [
+          ClipRRect(
+            borderRadius: BorderRadius.circular(20.0),
+            child: Image.network(
+              groupedMemory.img ?? '',
+              width: MediaQuery.of(context).size.width * 0.4,
+              height: MediaQuery.of(context).size.width * 0.4,
+              fit: BoxFit.cover,
+            ),
+          ),
+          const SizedBox(height: 8),
+          Text(
+            groupedMemory.imgTitle ?? '',
+            style: const TextStyle(
+                color: Colors.black,
+                fontSize: 24,
+                fontFamily: 'PretendardRegular'),
+            textAlign: TextAlign.center,
+          ),
+        ],
+      ),
+    );
+  }
+
+  Widget _buildBigMemoryCard(BuildContext context, MemoryNoteModel groupedMemory) {
+    return GestureDetector(
+      onTap: () => Get.to(MemoryDetail(memory: groupedMemory)),
+      child: Column(
+        children: [
+          ClipRRect(
+            borderRadius: BorderRadius.circular(20.0),
+            child: Image.network(
+              groupedMemory.img ?? '',
+              width: double.infinity,
+              height: MediaQuery.of(context).size.width * 0.4,
+              fit: BoxFit.cover,
+            ),
+          ),
+          const SizedBox(height: 8),
+          Text(
+            groupedMemory.imgTitle ?? '',
+            style: const TextStyle(
+                color: Colors.black,
+                fontSize: 24,
+                fontFamily: 'PretendardRegular'),
+            textAlign: TextAlign.center,
+          ),
+        ],
+      ),
+    );
+  }
+
 }
